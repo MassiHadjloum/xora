ca9a36ed48842a2d8c2c9c8af01bc9ea1ec6668c
massi <massi@MacBook-Air-de-massi.local>
2024-09-22 23:04:30 +0200

    add feature section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Features from "@/components/Features";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="overflow-hidden">
-        <Header  />
+    <main className="overflow-hidden bg-s1">
+      <Header />
+      <Hero />
+      <Features />
     </main>
-     );
+  );
 }
---
+++ lib/Types/index.ts
@@ -0,0 +1,13 @@
+export type TButton = {
+  icon: string;
+  title: string;
+};
+
+export type TFeature = {
+  id: string;
+  icon: string;
+  caption: string;
+  title: string;
+  text: string;
+  button: TButton;
+};
